5a297d48c4839884e71ddaf55f1cf622fa119294
Chris Mandich <mandichc@gmail.com>
2019-12-14 11:34:51 -0800

    Update minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyFlume",
-    version="0.2.2",
+    version="0.2.3",
     author="ChrisMandich",
     author_email="Chris@Mandich.net",
     description="Package to integrate with Flume Sensor",
