24489c17272bb1e150d4fca0d78070dbefc746c0
ChrisMandich <mandichc@gmail.com>
2020-05-01 16:12:38 -0700

    Update metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyFlume",
-    version="0.4.0",
+    version="0.5.0",
     author="ChrisMandich",
     author_email="Chris@Mandich.net",
     description="Package to integrate with Flume Sensor",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pytz", "pyjwt", "ratelimit",],
+    install_requires=["pyjwt", "ratelimit",],
 )
