698266640991f77917a2121b90d1bd772c98a968
cmandich <cmandich@rivian.com>
2023-08-17 23:20:27 +0900

    Cleanup isort.

--- pyflume/__init__.py
+++ pyflume/__init__.py
@@ -112,7 +112,7 @@ def _response_error(message, response):
         error_message = json.loads(response.text)['message']
 
     raise Exception(
-        "Message:{0}.\nResponse code returned:{1}.\nError message returned:{2}.".format(message, response.status_code, error_message),
+        'Message:{0}.\nResponse code returned:{1}.\nError message returned:{2}.'.format(message, response.status_code, error_message),
     )
 
 
