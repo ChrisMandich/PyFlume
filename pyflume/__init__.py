d4080ef51e89ea6dfc5d5e4246d69b740916c582
cmandich <cmandich@rivian.com>
2023-08-19 00:07:08 +0900

    Fix formatting.

--- pyflume/__init__.py
+++ pyflume/__init__.py
@@ -1,6 +1,6 @@
 """Authenticates to Flume API, returns a list of devices and allows you to pull the latest sensor results over a period of time."""
-from .auth import FlumeAuth  # noqa: WPS300
-from .data import FlumeData  # noqa: WPS300
-from .devices import FlumeDeviceList  # noqa: WPS300
-from .notifications import FlumeNotificationList  # noqa: WPS300
-from .usage import FlumeUsageAlertList  # noqa: WPS300
+from .auth import FlumeAuth  # noqa: WPS300, F401
+from .data import FlumeData  # noqa: WPS300, F401
+from .devices import FlumeDeviceList  # noqa: WPS300, F401
+from .notifications import FlumeNotificationList  # noqa: WPS300, F401
+from .usage import FlumeUsageAlertList  # noqa: WPS300, F401
