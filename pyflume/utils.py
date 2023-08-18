1f70a7de3290dbf29a3aaa3f1cfeb24b8a3e43ff
cmandich <cmandich@rivian.com>
2023-08-18 23:45:02 +0900

    Update Logger handler name.

--- pyflume/utils.py
+++ pyflume/utils.py
@@ -23,13 +23,13 @@ def configure_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    handler = logging.StreamHandler()
+    logger_handler = logging.StreamHandler()
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
-    handler.setFormatter(formatter)
+    logger_handler.setFormatter(formatter)
 
-    logger.addHandler(handler)
+    logger.addHandler(logger_handler)
 
     return logger
 
