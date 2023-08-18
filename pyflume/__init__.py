fc2492c00ebadfd5f445ba53e93667164decb192
cmandich <cmandich@rivian.com>
2023-08-18 22:37:50 +0900

    Fix formatting to support relative path.

--- pyflume/__init__.py
+++ pyflume/__init__.py
@@ -1,7 +1,7 @@
 """Authenticates to Flume API, returns a list of devices and allows you to pull the latest sensor results over a period of time."""
-from .auth import FlumeAuth
-from .data import FlumeData
-from .devices import FlumeDeviceList
-from .notifications import FlumeNotificationList
+from .auth import FlumeAuth  # noqa: WPS300
+from .data import FlumeData  # noqa: WPS300
+from .devices import FlumeDeviceList  # noqa: WPS300
+from .notifications import FlumeNotificationList  # noqa: WPS300
 
 __all__ = ['FlumeAuth', 'FlumeData', 'FlumeDeviceList', 'FlumeNotificationList']
